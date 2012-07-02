f57027bf6dbef3e10dce8c10325a9a994bd6d06e
Jon Packer <contact@jonpacker.com>
2012-07-02 23:15:23 +0200

    Initial commmit

---
+++ ipa.client.js
@@ -0,0 +1,33 @@
+var request = require('request');
+var path = require('path');
+var naan = require('naan')
+
+module.exports = function(serverLocation) {
+  var fnurl = function(path) {
+    return serverLocation + "/" + path;
+  }
+  var genericCallback = function(callback) {
+    return function(err, response, body) {
+      if (!callback) return;
+      if (err || response.status >= 300) {
+        callback(err || body || response.status);
+      } else {
+        callback();
+      }
+    }
+  }
+
+  var client = {
+    log: function ipaSendLogMessage(message, callback) {
+      request.post({
+        url: fnurl('log'),
+        json: { text: message }
+      }, genericCallback(callback));
+    },
+    logImage: function ipaSendLogImage(url, callback) {
+      client.log('<img src="' + url + '"/>', callback)
+    }
+  }
+
+  return client;
+}
